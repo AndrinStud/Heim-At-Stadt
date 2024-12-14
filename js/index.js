7fd683cea3eda17c67bd31dfb63d16f53cfc0cac
Andrin <andrinjosua.schaerli@gmail.com>
2024-12-14 03:40:03 +0100

    Wimmelbild ausgetauscht

--- js/index.js
+++ js/index.js
@@ -6,6 +6,18 @@ function redirectToSquattedHouse() {
     window.location.href = 'sites/Hausbesetzung.html';
 }
 
+function redirectToServicedApartments() {
+    window.location.href = 'sites/ServicedApartments.html';
+}
+
+function redirectToSmartCity() {
+    window.location.href = 'sites/SmartCity.html';
+}
+
+function redirectToPodcast() {
+    window.open('https://open.spotify.com/user/31qms57eosf6edpkozi6zww6g5dy?si=4272e7a2907343e5', '_blank');
+}
+
 window.onload = function() {
     //Schritt 1: Toggle Menü einrichten
     document.getElementById('toggleMenu').addEventListener('click', function() {
@@ -37,10 +49,17 @@ window.onload = function() {
         //Schritte 2: Variablen für die Bereiche definieren
         var defensiveArchitecture = document.getElementById('defensiveArchitecture');
         var squattedHouse = document.getElementById('squattedHouse');
+        var servicedApartments = document.getElementById('servicedApartments');
+        var smartCity = document.getElementById('smartCity');
+        var podcast = document.getElementById('podcast');
 
         // Schritt 3: Bildereiche definieren und Hover hinzufügen
         var isHoveringDefensiveArchitecture = false;
         var isHoveringSquattedHouse = false;
+        var isHoveringServicedApartments = false;
+        var isHoveringSmartCity = false;
+        var isHoveringPodcast = false;
+
         mainElement.addEventListener('mousemove', function(event) {
             var rect = mainElement.getBoundingClientRect();
             var x = event.clientX - rect.left; // x position within the element
@@ -49,8 +68,11 @@ window.onload = function() {
             var xPercent = (x / rect.width) * 100;
             var yPercent = (y / rect.height) * 100;
 
-            var isInDefensiveArchitecture = xPercent >= 1 && xPercent <= 31 && yPercent >= 62 && yPercent <= 88;
-            var isInSquattedHouse = xPercent >= 72 && xPercent <= 99 && yPercent >= 28 && yPercent <= 75;
+            var isInDefensiveArchitecture = xPercent >= 16 && xPercent <= 36 && yPercent >= 10 && yPercent <= 38;
+            var isInSquattedHouse = xPercent >= 65 && xPercent <= 87 && yPercent >= 39 && yPercent <= 74;
+            var isInServicedApartments = xPercent >= 40 && xPercent <= 52 && yPercent >= 11 && yPercent <= 34;
+            var isInSmartCity = xPercent >= 75 && xPercent <= 87 && yPercent >= 0 && yPercent <= 32;
+            var isInPodcast = xPercent >= 18 && xPercent <= 34 && yPercent >= 52 && yPercent <= 79;
 
             if (isInDefensiveArchitecture && !isHoveringDefensiveArchitecture) {
                 defensiveArchitecture.classList.add('defensiveArchitectureColored');
@@ -75,11 +97,51 @@ window.onload = function() {
                 mainElement.removeEventListener('click', redirectToSquattedHouse);
                 mainElement.style.cursor = 'default'; // Change cursor back to default
             }
+
+            if (isInServicedApartments && !isHoveringServicedApartments) {
+                servicedApartments.classList.add('servicedApartmentsColored');
+                isHoveringServicedApartments = true;
+                mainElement.addEventListener('click', redirectToServicedApartments);
+                mainElement.style.cursor = 'pointer';
+            } else if (!isInServicedApartments && isHoveringServicedApartments) {
+                servicedApartments.classList.remove('servicedApartmentsColored');
+                isHoveringServicedApartments = false;
+                mainElement.removeEventListener('click', redirectToServicedApartments);
+                mainElement.style.cursor = 'default';
+            }
+
+            if (isInSmartCity && !isHoveringSmartCity) {
+                smartCity.classList.add('smartCityColored');
+                isHoveringSmartCity = true;
+                mainElement.addEventListener('click', redirectToSmartCity);
+                mainElement.style.cursor = 'pointer';
+            } else if (!isInSmartCity && isHoveringSmartCity) {
+                smartCity.classList.remove('smartCityColored');
+                isHoveringSmartCity = false;
+                mainElement.removeEventListener('click', redirectToSmartCity);
+                mainElement.style.cursor = 'default';
+            }
+
+            if (isInPodcast && !isHoveringPodcast) {
+                podcast.classList.add('podcastColored');
+                isHoveringPodcast = true;
+                mainElement.addEventListener('click', redirectToPodcast);
+                mainElement.style.cursor = 'pointer';
+            } else if (!isInPodcast && isHoveringPodcast) {
+                podcast.classList.remove('podcastColored');
+                isHoveringPodcast = false;
+                mainElement.removeEventListener('click', redirectToPodcast);
+                mainElement.style.cursor = 'default';
+            }
         });
 
         // Schritt 4: Navigation definieren und Hover hinzufügen
         var navDefensiveArchitecture = document.getElementById('navDefensiveArchitecture');
         var navSquattedHouse = document.getElementById('navSquattedHouse');
+        var navServicedApartments = document.getElementById('navServicedApartments');
+        var navSmartCity = document.getElementById('navSmartCity');
+        var navPodcast = document.getElementById('navPodcast');
+
         navDefensiveArchitecture.addEventListener('mouseover', function() {
             defensiveArchitecture.classList.add('defensiveArchitectureColored');
         });
@@ -91,6 +153,24 @@ window.onload = function() {
         });
         navSquattedHouse.addEventListener('mouseout', function() {
             squattedHouse.classList.remove('squattedHouseColored');
+        });
+        navServicedApartments.addEventListener('mouseover', function() {
+            servicedApartments.classList.add('servicedApartmentsColored');
+        });
+        navServicedApartments.addEventListener('mouseout', function() {
+            servicedApartments.classList.remove('servicedApartmentsColored');
+        });
+        navSmartCity.addEventListener('mouseover', function() {
+            smartCity.classList.add('smartCityColored');
+        });
+        navSmartCity.addEventListener('mouseout', function() {
+            smartCity.classList.remove('smartCityColored');
+        });
+        navPodcast.addEventListener('mouseover', function() {
+            podcast.classList.add('podcastColored');
+        });
+        navPodcast.addEventListener('mouseout', function() {
+            podcast.classList.remove('podcastColored');
         });
     };
 };
